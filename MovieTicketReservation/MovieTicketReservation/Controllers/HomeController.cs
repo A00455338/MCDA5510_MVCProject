634a1614654cad6dc518c2f7e29fa14730b8d18e
A00455338 <navdeep.kaur.navdeep.kaur@smu.ca>
2021-11-17 23:32:34 -0400

    Designed a Home Page and a Login Page

---
+++ MovieTicketReservation/MovieTicketReservation/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketReservation.Controllers
+{
+    public class HomeController : Controller
+    {
+       [Route("")]
+       public IActionResult HomePage()
+        {
+            return View("MainPage");
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult LoginUser()
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieTicketReservation/MovieTicketReservation/Models/MoviesModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketReservation.Models
+{
+    public class MoviesModel
+    {
+        public int movieId { get; set; }
+        public string movieName { get; set; }
+        public string movieGenre { get; set; }
+
+    }
+}
