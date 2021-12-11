4c5c92c117f118a8647b4b89f594937de3faa30a
aamirmansuri <Aamir.Faruk.Mansuri@smu.ca>
2021-12-11 19:37:02 -0400

    Added Theatre Model

---
+++ MovieTicketReservation/MovieTicketReservation/Models/TheatreModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketReservation.Models
+{
+    public class TheatreModel
+    {
+        public int theatreId { get; set; }
+        public string theatreName { get; set; }
+        public string address { get; set; }
+    }
+}
