386597ee6c5fa9166d8ff41b6c57782347f6aad8
aamirmansuri <Aamir.Faruk.Mansuri@smu.ca>
2021-12-13 20:27:41 -0400

    Changes for validation

--- MovieTicketReservation/MovieTicketReservation/Models/CustomerModel.cs
+++ MovieTicketReservation/MovieTicketReservation/Models/CustomerModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,10 @@ namespace MovieTicketReservation.Models
         public int userId { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage ="Please enter a valid Email Address.")]
         public string Email { get; set; }
+
         public string Password { get; set; }
 
     }
